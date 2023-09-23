cfbaceb7a8df2acd30ab2439c424daefbd9d0ac6
dev_gulmira <mammadovagulmira077@gmail.com>
2023-09-23 21:25:00 +0400

    d

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      Styled-components <span>.attrs</span> method
+      <p>jsndj</p>
+      <p>jsndj</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/layout/header/Header.tsx
@@ -0,0 +1,19 @@
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <a href="#">
+          <img src="" />
+        </a>
+        <p>Behoof</p>
+        <p>Лучшие цены в интернет-магазинах</p>
+      </div>
+      <button>
+        <img />
+      </button>
+      <input />
+    </header>
+  );
+};
+
+export default Header;
